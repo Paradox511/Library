a326aaa15c7775324fc7cd689b57edf1c0578f92
Huynh Lam Duc <61283529+Paradox511@users.noreply.github.com>
2023-04-08 10:37:40 +0700

    Add files via upload

---
+++ Base/Employee.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package library;
+import java.util.*;
+import java.lang.*;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Employee {
+    String id,ho,ten;
+    String ngaysinh,gioitinh,sdt;
+    float luong;
+    
+  public String getid(){
+      return id;
+  }
+  public void setid(String id){
+      this.id = id;
+  }
+    public String getho(){
+      return ho;
+  }
+  public void setho(String ho){
+      this.ho = ho;
+  }
+    public String getten(){
+      return ten;
+  }
+  public void setten(String ten){
+      this.ten = ten;
+  }
+    public String getngaysinh(){
+      return ngaysinh;
+  }
+  public void setngaysinh(String ngaysinh){
+      this.ngaysinh = ngaysinh;
+  }
+    public String getgioitinh(){
+      return gioitinh;
+  }
+  public void setgioitinh(String gioitinh){
+      this.gioitinh = gioitinh;
+  }
+    public String getsdt(){
+      return sdt;
+  }
+  public void setsdt(String sdt){
+      this.sdt = sdt;
+  }
+  public float getluong(){
+      return luong;
+  }
+  public void setluong(float luong){
+      this.luong = luong;
+  }
+}
