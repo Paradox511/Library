f6b20cf655c4b3a92cf3d826fd6c5fbe90adae60
Paradox511 <61283529+Paradox511@users.noreply.github.com>
2023-04-08 07:40:35 +0700

    Add files via upload

---
+++ Base/Reader.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */package library;
+import java.util.*;
+import java.lang.*;
+
+
+/**
+ *
+ * @author ADMIN
+ */
+public class Reader {
+    String id,ho,ten;
+    String ngaysinh,gioitinh,sdt;
+    
+  public String getid(){
+      return id;
+  }
+  public void setid(String id){
+      this.id = id;
+  }
+    public String getho(){
+      return ho;
+  }
+  public void setho(String ho){
+      this.ho = ho;
+  }
+    public String getten(){
+      return ten;
+  }
+  public void setten(String ten){
+      this.ten = ten;
+  }
+    public String getngaysinh(){
+      return ngaysinh;
+  }
+  public void setngaysinh(String ngaysinh){
+      this.ngaysinh = ngaysinh;
+  }
+    public String getgioitinh(){
+      return gioitinh;
+  }
+  public void setgioitinh(String gioitinh){
+      this.gioitinh = gioitinh;
+  }
+    public String getsdt(){
+      return sdt;
+  }
+  public void setsdt(String sdt){
+      this.sdt = sdt;
+  }
+}
